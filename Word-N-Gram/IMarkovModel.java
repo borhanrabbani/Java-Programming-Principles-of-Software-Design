564df0e8d32259b219ebe8e636d87f1f80b2bb3b
Borhan Rabbani <borhanuddinse28@gmail.com>
2022-05-15 22:24:39 +0600

    Add files via upload

---
+++ Word-N-Gram/IMarkovModel.java
@@ -0,0 +1,16 @@
+package com.week3.wordNGrams;
+/**
+ * Write a description of interface IMarkovModel here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public interface IMarkovModel {
+    public void setTraining(String text);
+    
+    public void setRandom(int seed);
+    
+    public String getRandomText(int numChars);
+
+}
